29a8d2adfda2d6c0b044b08f2662cf0b15cdab11
Mr1cecream <guy@ochakov.com>
2022-06-29 21:42:44 +0300

    Added project.nvim

--- lua/plugconf/nvimtree.lua
+++ lua/plugconf/nvimtree.lua
@@ -1,1 +1,8 @@
-PSetup('nvim-tree')
+PSetup('nvim-tree', {
+    update_cwd = true,
+    respect_buf_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+    },
+})
--- lua/plugconf/telescope.lua
+++ lua/plugconf/telescope.lua
@@ -1,12 +1,18 @@
+local function PLoadExtension(telescope, extension)
+    local ok = pcall(telescope.load_extension, extension)
+    if not ok then
+        print('Failed to load Telescope extension: howdoi')
+    end
+end
+
 local ok, telescope = pcall(require, 'telescope')
 if not ok then
     print('Failed to load Telescope')
     return
 end
-ok = pcall(telescope.load_extension, 'howdoi')
-if not ok then
-    print('Failed to load Telescope extension: howdoi')
-end
+
+PLoadExtension(telescope, 'howdoi')
+PLoadExtension(telescope, 'projects')
 
 -- require('telescope').setup {
 -- extensions = {
