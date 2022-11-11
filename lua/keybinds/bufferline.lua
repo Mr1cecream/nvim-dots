e545052a931a7b44bc5dc25694bf9399d82c5474
Mr1cecream <guy@ochakov.com>
2022-11-11 12:28:48 +0200

    Fix BufferLine cycle keybinds

--- lua/keybinds/bufferline.lua
+++ lua/keybinds/bufferline.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap('n', '<Leader>n', '<cmd>BufferLineCycleNext<CR>', opts)
+keymap('n', '<Leader>o', '<cmd>BufferLineCycleNext<CR>', opts)
 keymap('n', '<Leader>n', '<cmd>BufferLineCyclePrev<CR>', opts)
-keymap('n', '<Leader>q', '<cmd>bdelete!<CR><cmd>NvimTreeClose<CR><cmd>BufferLineCycleNext<CR>', opts)
+keymap('n', '<Leader>q', '<cmd>bdelete!<CR><CMD>NvimTreeClose<CR><cmd>BufferLineCycleNext<CR>', opts)
