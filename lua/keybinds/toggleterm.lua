00429999b725d3ff602d8dbcbed00003fd06ee4f
Mr1cecream <guy@ochakov.com>
2022-12-24 15:26:07 +0200

    Fix toggleterm keybind

--- lua/keybinds/toggleterm.lua
+++ lua/keybinds/toggleterm.lua
@@ -1,4 +1,4 @@
 local keymap = vim.keymap.set
 
 keymap('t', '<Esc>', '<C-\\><C-n><C-w>k', { noremap = true, silent = true })
-keymap('n', '<Leader>p', '<CMD>ToggleTerm')
+keymap('n', '<Leader>p', '<CMD>ToggleTerm<CR>')
