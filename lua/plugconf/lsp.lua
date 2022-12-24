de0d6c0c9e8abd5a1906156d6111bdbecbd762fe
Mr1cecream <guy@ochakov.com>
2022-12-24 16:05:47 +0200

    Add automatic setup for null-ls with mason and tidy up code

--- lua/plugconf/lsp.lua
+++ lua/plugconf/lsp.lua
@@ -1,61 +1,57 @@
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities
+PRequire('cmp_nvim_lsp', function(lsp)
+    capabilities = lsp.default_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+end)
 
-local lsp_signature
-ok, lsp_signature = pcall(require, 'lsp_signature')
-if not ok then
-	print('Failed to load lsp-signature')
-	return
-end
+local lsp_signature = PRequire('lsp_signature')
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-	lsp_signature.on_attach({
-		bind = true,
-		handler_opts = {
-			border = 'rounded',
-		},
-	}, bufnr)
-	if client.supports_method('textDocument/formatting') then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd('BufWritePre', {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({ bufnr = bufnr })
-			end,
-		})
-	end
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    if lsp_signature ~= nil then
+        lsp_signature.on_attach({
+            bind = true,
+            handler_opts = {
+                border = 'rounded',
+            },
+        }, bufnr)
+    end
+    if client.supports_method('textDocument/formatting') then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+    end
 end
 
 PSetup('mason')
-PSetup('mason-lspconfig', {
-	ensure_installed = { 'sumneko_lua', 'rust_analyzer', 'rustfmt' }, -- automatically install lua and rust LSPs
-})
+PSetup('mason-lspconfig')
 
-local null_ls = PRequire('null-ls')
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.formatting.stylua,
-	},
-	on_attach = on_attach,
-})
+PRequire('mason-null-ls', function(mason_null_ls)
+    mason_null_ls.setup({
+        automatic_installation = false,
+        automatic_setup = true,
+    })
+    PSetup('null-ls', {
+        on_attach = on_attach,
+    })
+    mason_null_ls.setup_handlers()
+end)
 
-local lspconfig
-ok, lspconfig = pcall(require, 'lspconfig')
-if not ok then
-	print('Failed to load lspconfig')
-	return
-end
-local servers = { 'rust_analyzer', 'sumneko_lua', 'csharp_ls', 'bashls', 'html', 'clangd' }
-for _, lsp in pairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-		flags = {
-			debounce_text_changes = 150,
-		},
-	})
-end
+PRequire('lspconfig', function(lspconfig)
+    local servers = { 'rust_analyzer', 'sumneko_lua', 'csharp_ls', 'bashls', 'html', 'clangd' }
+    for _, lsp in pairs(servers) do
+        lspconfig[lsp].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = {
+                debounce_text_changes = 150,
+            },
+        })
+    end
+end)
