92e43a90d4a967c461fa705a9b76c5045ac12165
Mr1cecream <guy@ochakov.com>
2022-12-24 16:05:18 +0200

    Add callback to PRequire function and default table for PSetup

--- init.lua
+++ init.lua
@@ -2,24 +2,28 @@ vim = vim
 
 --- Protected require
 ---@param module string
+---@param[opt=nil] callback function
 ---@return table
-function PRequire(module)
-	local ok, loaded = pcall(require, module)
-	if not ok then
-		print('Failed to load ' .. module)
-		return {}
-	end
-	return loaded
+function PRequire(module, callback)
+    local ok, loaded = pcall(require, module)
+    if not ok then
+        print('Failed to load ' .. module)
+        return nil
+    end
+    if callback ~= nil then
+        callback(loaded)
+    end
+    return loaded
 end
 
 local modules = {
-	'opts', -- load options module
-	'plugins', -- load plugins module
-	'colors', -- load colors module
-	'plugconf', -- load plugin configs module
-	'keybinds', -- load keybinds module
+    'opts', -- load options module
+    'plugins', -- load plugins module
+    'colors', -- load colors module
+    'plugconf', -- load plugin configs module
+    'keybinds', -- load keybinds module
 }
 
 for _, mod in ipairs(modules) do
-	require(mod)
+    require(mod)
 end
--- lua/plugconf/init.lua
+++ lua/plugconf/init.lua
@@ -2,39 +2,39 @@ require('impatient')
 
 --- Protected require and setup
 ---@param module string
----@param config any
+---@param[opt=nil] config table
 function PSetup(module, config)
-	if module == nil then
-		return
-	end
-	local ok, loaded = pcall(require, module)
-	if not ok then
-		print('Failed to load ' .. module)
-		return
-	end
-	loaded.setup(config)
+    if module == nil then
+        return
+    end
+    local ok, loaded = pcall(require, module)
+    if not ok then
+        print('Failed to load ' .. module)
+        return
+    end
+    loaded.setup(config)
 end
 
 local configs = {
-	'autopairs', -- load autopairs config module
-	'better-escape', -- load better-escape.nvim config module
-	'bufferline', -- load bufferline config module
-	'cmp', -- load cmp config module
-	'lsp', -- load nvim-lsp config module
-	'lualine', -- load lualine config module
-	'neorg', -- load neorg config module
-	'neoscroll', -- load neoscroll config module
-	'numb', -- load numb.nvim config module
-	'nvimtree', -- load nvim-tree config module
-	'popui', -- load popui.nvim config module
-	'telescope', -- load telescope config module
-	'toggleterm', -- load toggleterm config module
-	'treesitter', -- load treesitter.nvim config module
-	'trouble', -- load trouble.nvim config module
+    'autopairs', -- load autopairs config module
+    'better-escape', -- load better-escape.nvim config module
+    'bufferline', -- load bufferline config module
+    'cmp', -- load cmp config module
+    'lsp', -- load nvim-lsp config module
+    'lualine', -- load lualine config module
+    'neorg', -- load neorg config module
+    'neoscroll', -- load neoscroll config module
+    'numb', -- load numb.nvim config module
+    'nvimtree', -- load nvim-tree config module
+    'popui', -- load popui.nvim config module
+    'telescope', -- load telescope config module
+    'toggleterm', -- load toggleterm config module
+    'treesitter', -- load treesitter.nvim config module
+    'trouble', -- load trouble.nvim config module
 }
 
 for _, conf in ipairs(configs) do
-	PRequire('plugconf/' .. conf)
+    PRequire('plugconf/' .. conf)
 end
 
 PSetup('hop')
